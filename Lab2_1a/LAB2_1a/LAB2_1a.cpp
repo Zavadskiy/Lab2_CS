7bafbbb13777283b5632b81555e666b256e94208
Zavadskyi Viacheslav <45971262+Zavadskiy@users.noreply.github.com>
2021-06-23 20:44:36 +0300

    Lab2
    first, second

---
+++ Lab2_1a/LAB2_1a/LAB2_1a.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <bitset> // Для бітового представлення
+#include <cstdlib> // Для української мови у консолі
+using namespace std;
+
+
+int multiply(int x, int y)
+{
+    int ansver = 0, count = 0;
+    bitset<16> bs1(x);
+    bitset<16> bs2(y);
+    std::cout << "Перший множник у двійковій системі " << bs1 << endl;
+    std::cout << "Друге множник  у двійковій системі " << bs2 << endl;
+    std::cout << endl;
+    while (y) // цикл множення
+    {
+        if (y % 2 == 1)
+            ansver += x << count;
+        bitset<16> bs0(ansver);
+        std::cout << "Результат   = " << bs0 << endl;
+        count++;
+        y /= 2;
+        bitset<16> bs1(x);
+        bitset<16> bs2(y);
+        std::cout << "Перший множник = " << bs1 << endl;
+        std::cout << "Друге множник  = " << bs2 << endl;
+        std::cout << endl;
+
+    }
+    return ansver;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    int x, y;
+    std::cout << " Число перше = ";
+    std::cin >> x;
+    std::cout << " Число друге = ";
+    std::cin >> y;
+    cout << multiply(x, y) << endl;
+    return 0;
+}
---
+++ Lab2_1a/LAB2_2b/Lab2_2b/Lab2_2b.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <bitset>
+#include <cstdlib>
+
+using namespace std;
+
+int division(int r, int d)
+{
+    int q = 0;
+    d = d << 32;
+    r = r << 1;
+    for (int i = 0; i < 32; i++)
+    {
+        std::cout << i << endl;
+        if (r <= d)
+        {
+            std::cout << "Дільник меньший за ділене" << endl;
+            r = r - d;
+            std::cout << "remainder << 1 " << endl;
+            r = r << 1;
+            std::cout << "quantient << 1 " << endl;
+            q = q << 1;
+            std::cout << "quantient | 1" << endl;
+            q = q | 1;
+        }
+        else
+        {
+            std::cout << "Дільник більший за ділене" << endl;
+            std::cout << "remainder << 1" << endl;
+            r = r << 1;
+            std::cout << "quantient << 1" << endl;
+            q = q << 1;
+        }
+        bitset<64> bs1(r);
+        bitset<32> bs2(d >> 32);
+        bitset<32> bs3(q);
+        std::cout << "Ділене  = " << bs1 << endl;
+        std::cout << "Частка  = " << bs3 << endl;
+        std::cout << "Дільник  = " << bs2 << endl;
+        std::cout << endl;
+    }
+    return  q;
+}
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    system("chcp 1251");
+    int remainder, divisor;
+    std::cout << " Ділене = ";
+    std::cin >> remainder;
+    std::cout << " Дільник = ";
+    std::cin >> divisor;
+    cout << division(remainder, divisor);
+    return 0;
+}
